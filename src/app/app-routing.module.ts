ba8185430fc352d60f3bc49a7218110afc7f378f
ivan.monahhov@gmail.com <Rkka 1945>
2021-02-18 15:57:16 +0200

    backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   {path:"cart", component: CartComponent},
+  {path:"", component: HomeComponent},
   {path:"**", redirectTo:"/"}
 ];
 
